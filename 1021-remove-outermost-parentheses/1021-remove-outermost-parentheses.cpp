1bf7f5e351b65a2a264167a20165e4a577166356
Uplakshay Vishwakarma <shivammda113@gmail.com>
2026-07-14 00:05:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.79%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        int balance = 0;
+        for(char ch : s){
+            if(ch == '('){
+                if(balance > 0) result += ch;
+                balance++;
+            }else if(ch == ')'){
+                balance--;
+                if(balance > 0) result += ch;
+            }
+        }
+        return result;
+    }
+};
